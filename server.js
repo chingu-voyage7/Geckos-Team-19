d47d0f198dd1c8f56d5bd2fdcacba1e8f41960f2
Martin Gebauer <gebec.m@gmail.com>
2018-12-03 16:43:33 +0100

    #1: Fixed db connection check

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const appPort = parseInt(process.env.PORT, 10) || 3000;
 const app = next({ dev });
 const {login, password, host, port, database} = require('./config/database.js');
 
-app.prepare().then(() => {
-  const server = express();
-  const db = pgp(`postgres://${login}:${password}@${host}:${port}/${database}`);
+const db = pgp(`postgres://${login}:${password}@${host}:${port}/${database}`);
+db.connect()
+  .then(r => { // TODO: rename and use the param
+    app.prepare().then(() => {
+      const server = express();
 
-  if (db) {
-    console.log('Connection to database established!');
-  } else {
-    console.log('Error while connecting to database!');
-  }
+      Router.forEachPrettyPattern((page, pattern, defaultParams) => {
+        server.get(pattern, (req, res) => {
+          app.render(req, res, `/${page}`, Object.assign({}, defaultParams, req.query, req.params));
+        });
+      });
 
-  Router.forEachPattern((page, pattern, defaultParams) => {
-    server.get(pattern, (req, res) => {
-      app.render(req, res, `/${page}`, Object.assign({}, defaultParams, req.query, req.params));
-    });
+      server.listen(appPort, () => `Listening on ${appPort}`);
+    })
+  })
+  .catch(err => {
+    console.log(`Error while connecting to database! Error: ${err}`);
   });
-
-  server.listen(appPort, () => `Listening on ${appPort}`);
-})
